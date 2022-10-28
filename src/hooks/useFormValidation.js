b05ab7c0eb0f7cf9bff250921f4d100c0a312b92
Julia <JLHartsek@gmail.com>
2022-10-28 11:41:31 -0400

    feature: add SignUpForm component

---
+++ src/hooks/useFormValidation.js
@@ -0,0 +1,30 @@
+import React, { useCallback } from 'react';
+
+export function useFormValidation() {
+  const [inputValues, setInputValues] = React.useState({});
+  const [errors, setErrors] = React.useState({});
+  const [isValid, setIsValid] = React.useState(false);
+
+  function handleChange(e) {
+    const form = e.target.closest('.form');
+    const { name, value } = e.target;
+    setInputValues({ ...inputValues, [name]: value });
+    setErrors({ ...errors, [name]: e.target.validationMessage });
+    setIsValid(form.checkValidity());
+  }
+
+  const resetForm = useCallback(() => {
+    setInputValues({});
+    setErrors({});
+    setIsValid(false);
+  }, [setInputValues, setErrors, setIsValid]);
+
+  return {
+    inputValues,
+    errors,
+    isValid,
+    handleChange,
+    setInputValues,
+    resetForm,
+  };
+}

7d3ee0fba4d08b048f442519f5afbe354633e7de
Julia <JLHartsek@gmail.com>
2022-11-02 23:04:34 -0400

    fix: update footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,24 +12,36 @@ function Footer() {
       <nav className='footer__nav nav'>
         <ul className='nav__text text'>
           <li className='text__link'>
-            <a className='text__link' href='/'>
+            <a className='text__link' href='/' target='_blank'>
               Home
             </a>
           </li>
           <li className='text__link'>
-            <a className='text__link' href='/'>
+            <a
+              className='text__link'
+              href='https://practicum.com/'
+              target='_blank'
+            >
               Practicum
             </a>
           </li>
         </ul>
         <ul className='nav__icons icons'>
           <li className='icons__link'>
-            <a className='icons__link' href='/'>
+            <a
+              className='icons__link'
+              href='https://www.facebook.com/'
+              target='_blank'
+            >
               <img src={facebook} alt='facebook logo' />
             </a>
           </li>
           <li className='icons__link'>
-            <a className='icons__link' href='/'>
+            <a
+              className='icons__link'
+              href='https://github.com/JHartsek'
+              target='_blank'
+            >
               <img src={gitHub} alt='github logo' />
             </a>
           </li>

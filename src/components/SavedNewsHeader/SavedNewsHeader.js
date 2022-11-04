23633f7b4ec3371d69b54d67a337ff8f19d6f744
Julia <JLHartsek@gmail.com>
2022-11-04 10:36:30 -0400

    fix: adjust savedNewsHeader header tags

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -3,16 +3,16 @@ import './SavedNewsHeader.css';
 function SavedNewsHeader() {
   return (
     <section className='saved-news-header'>
-      <h3 className='saved-news-header__tag'>Saved articles</h3>
-      <h2 className='saved-news-header__title'>
+      <h2 className='saved-news-header__tag'>Saved articles</h2>
+      <h1 className='saved-news-header__title'>
         Elise, you have 5 saved articles
-      </h2>
-      <h3 className='saved-news-header__details'>
+      </h1>
+      <h2 className='saved-news-header__details'>
         By keywords:{' '}
         <span className='saved-news-header__details-keywords'>
           Nature, Yellowstone, and 2 others
         </span>
-      </h3>
+      </h2>
     </section>
   );
 }

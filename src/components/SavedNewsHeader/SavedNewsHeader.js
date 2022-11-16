5524f91d81521ca415b3d63ab7cc878454e8ee2b
Julia <JLHartsek@gmail.com>
2022-11-16 11:21:45 -0500

    udpate savedNewsHeader with articles count

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -3,20 +3,18 @@ import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
 import './SavedNewsHeader.css';
 
-function SavedNewsHeader() {
+function SavedNewsHeader({ savedCards }) {
   const { name } = React.useContext(CurrentUserContext);
 
   return (
     <section className='saved-news-header'>
       <h2 className='saved-news-header__tag'>Saved articles</h2>
       <h1 className='saved-news-header__title'>
-        {`${name}, you have 5 saved articles`}
+        {`${name}, you have ${savedCards.length} saved articles`}
       </h1>
       <h2 className='saved-news-header__details'>
         By keywords:{' '}
-        <span className='saved-news-header__details-keywords'>
-          Nature, Yellowstone, and 2 others
-        </span>
+        <span className='saved-news-header__details-keywords'></span>
       </h2>
     </section>
   );

8832af7b8d58bd1b36e9be5ad7ce14a154d10fee
Julia <JLHartsek@gmail.com>
2022-11-17 10:01:44 -0500

    update keyword display

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -3,8 +3,27 @@ import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
 import './SavedNewsHeader.css';
 
-function SavedNewsHeader({ savedCards }) {
+function SavedNewsHeader({ savedCards, sortedKeywords }) {
   const { name } = React.useContext(CurrentUserContext);
+
+  const displayKeywords = () => {
+    if (sortedKeywords.length <= 3) {
+      switch (sortedKeywords.length) {
+        case 1:
+          return `${sortedKeywords[0]}`;
+        case 2:
+          return `${sortedKeywords[0]} and ${sortedKeywords[1]}`;
+        case 3:
+          return `${sortedKeywords[0]}, ${sortedKeywords[1]}, and ${sortedKeywords[2]}`;
+        default:
+          return '';
+      }
+    } else {
+      return `${sortedKeywords[0]}, ${sortedKeywords[1]}, and ${
+        sortedKeywords.length - 2
+      } more`;
+    }
+  };
 
   return (
     <section className='saved-news-header'>
@@ -14,7 +33,9 @@ function SavedNewsHeader({ savedCards }) {
       </h1>
       <h2 className='saved-news-header__details'>
         By keywords:{' '}
-        <span className='saved-news-header__details-keywords'></span>
+        <span className='saved-news-header__details-keywords'>
+          {displayKeywords()}
+        </span>
       </h2>
     </section>
   );
